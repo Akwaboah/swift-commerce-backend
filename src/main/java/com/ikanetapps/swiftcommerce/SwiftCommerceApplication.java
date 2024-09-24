43c969463cb19fb9faf46cd1541327e34e7bca03
Akwaboah <akwaboahisaac22@gmail.com>
2024-09-24 02:37:06 -0700

    chore: Project init

---
+++ src/main/java/com/ikanetapps/swiftcommerce/SwiftCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ikanetapps.swiftcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ikanetapps/swiftcommerce/SwiftCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikanetapps.swiftcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
